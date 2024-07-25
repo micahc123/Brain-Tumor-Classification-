dc13400051a45693f56c8568fb127e09d8e865bf
Micah C <106202832+cheesebuilding@users.noreply.github.com>
2024-07-25 12:37:41 +0800

    initial commit

---
+++ Brain-Tumor-Dataset-1/README.dataset.txt
@@ -0,0 +1,6 @@
+# Brain Tumor Dataset > 2024-07-22 3:27am
+https://universe.roboflow.com/mc-rb0d2/brain-tumor-dataset-yy1zw
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ main.py
@@ -0,0 +1,29 @@
+from ultralytics import YOLO
+import numpy as np
+import os
+import cv2  
+
+model = YOLO("runs/classify/train/weights/best.pt")
+
+folder_input_path = "test_imgs"
+folder_output_path = "annotated_imgs"  
+
+if not os.path.exists(folder_output_path):
+    os.makedirs(folder_output_path)
+
+for file_name in os.listdir(folder_input_path):
+    file_path = os.path.join(folder_input_path, file_name)
+    results = model.predict(source=file_path)
+
+    probs = results[0].probs
+    score = round(float(probs.top1conf.cpu().numpy()), 2)
+    text = f"{model.names[probs.top1]} {score}"
+
+    img = cv2.imread(file_path)
+
+    img = cv2.putText(img, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 225, 225))
+
+    output_file_path = os.path.join(folder_output_path, file_name)
+    cv2.imwrite(output_file_path, img)
+
+    print(f"Annotated image saved to {output_file_path}")
